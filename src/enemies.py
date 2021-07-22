82c4921f991f74aaa3ed9c66ae46386c9e6f7487
ben3304 <benjamin.johnston43@education.nsw.gov.au>
2021-07-22 11:00:37 +1000

    Update enemies.py

--- src/enemies.py
+++ src/enemies.py
@@ -1,7 +1,8 @@
 import pygame
 import random
 from entity import Entity
-
+from player import Player
+player = Player()
 
 class Enemy(Entity):
     def __init__(self):
@@ -9,7 +10,10 @@ class Enemy(Entity):
         self.surf = pygame.surface((30, 10))
         self.surf.fill((255, 0, 0))
         self.rect = self.surf.get_rect(
-            center = (300, 100)
+            center = (
+                random.randint(150, 200),
+                random.randint(0,150)
+            )
         )
         self.speed = random.randint(7,15)
 
@@ -18,4 +22,11 @@ def update(self):
 
 if pygame.sprite.spritecollideany(Player, Enemy):
     Player.kill()
-    running = false
+    running = False
+
+enemies = pygame.sprite.Group()
+all_sprites = pygame.sprite.Group()
+all_sprites.add(player)
+
+for entity in all_sprites:
+    pygame.surface.blit(entity.surf, entity.rect)
